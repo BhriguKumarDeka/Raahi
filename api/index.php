8d95c1068e341d062bef1cd7c265021c09a5724d
BhriguKD <vrigukumar710@gmail.com>
2026-05-26 21:58:07 +0530

    Fix config cache override

--- api/index.php
+++ api/index.php
@@ -21,9 +21,6 @@ try {
     putenv('VIEW_COMPILED_PATH=' . $storageDir . '/framework/views');
     putenv('APP_SERVICES_CACHE=' . '/tmp/services.php');
     putenv('APP_PACKAGES_CACHE=' . '/tmp/packages.php');
-    putenv('APP_CONFIG_CACHE=' . '/tmp/config.php');
-    putenv('APP_ROUTES_CACHE=' . '/tmp/routes.php');
-    putenv('APP_EVENTS_CACHE=' . '/tmp/events.php');
 
     // Forward Vercel requests to the normal Laravel entry point
     require __DIR__ . '/../public/index.php';
